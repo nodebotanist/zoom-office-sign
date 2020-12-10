5ba7b8979b9314bf9aec39024c7b2ed34952c627
Kassian Wren <the@nodebotani.st>
2020-12-10 13:53:11 -0600

    Writing text test

--- python-code/doorsign.py
+++ python-code/doorsign.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from inky.inky_uc8159 import Inky
-from font_source_sans_pro import SourceSansProSemiBold
+from PIL import Image, ImageFont, ImageDraw
+from font_source_serif_pro import SourceSerifProSemibold
+from font_source_sans_pro import SourceSansProSemibold
 
 inky = Inky()
 
@@ -9,5 +11,9 @@ for y in range(inky.height - 1):
     color = y // (inky.height // 7)
     for x in range(inky.width - 1):
         inky.set_pixel(x, y, color)
+
+font = ImageFont.truetype(SourceSerifProSemibold, font_size)
+
+draw.multiline_text((0, 0), "Hello World!", fill=inky_display.WHITE, font=font, align="left")
 
 inky.show()
